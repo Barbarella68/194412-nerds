ad19d9346dafcb02124a1269a1f8fe8fa569eb5b
Barbarella68 <gnkat@mail.ru>
2016-09-10 01:19:44 +0300

    анимация модального окна, интерактивная карта

--- js/script.js
+++ js/script.js
@@ -10,4 +10,19 @@ close.addEventListener("click", function(event) {
   event.preventDefault();         
   open.classList.remove("modal-content-show");
 });  
+function initialize() {
+  var mapOptions = {
+    zoom: 17,
+    center: new google.maps.LatLng(59.939, 30.320)
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  var image = "img/index/marker-logotype.png";
+  var myLatLng = new google.maps.LatLng(59.93865, 30.32365);
+  var nerdsMarker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      icon: image
+  });
+}
+google.maps.event.addDomListener(window, "load", initialize);
     
